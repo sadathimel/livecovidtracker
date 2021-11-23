47258466688c4f166da90e770864d854ace04b65
Mahadi001 <sadat.himel@gmail.com>
2021-11-23 22:09:42 +0600

    update

---
+++ src/components/Covid.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Covid = () => {
+    return (
+      <>
+        <h1>LIVE</h1>
+        <h2>COVID-19 CORONAVIRUS TRACER</h2>
+      </>
+    );
+}
+
+export default Covid
